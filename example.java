531c0b290e82d9a8a10e613b578f6942aea402f1
rajeshpadashetty2 <168393548+rajeshpadashetty2@users.noreply.github.com>
2024-04-29 09:29:56 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class example {
+   public static void main(String[] args) {
+       Scanner Sc = new Scanner(System.in);
+       int n = Sc.nextInt();
+      
+       for(int i=1; i<11;i++){
+        System.out.println(i*n);
+       }
+   } 
+}
